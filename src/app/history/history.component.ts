785a3c99d648a2f6402aa2259ff1994374ede9c8
Damian <damianspizzirri@gmail.com>
2019-12-27 19:40:09 -0300

    Obtiene imagen de historia desde firebase

--- src/app/history/history.component.ts
+++ src/app/history/history.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { FirebaseService } from '../transversal/services/firebase.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-history',
@@ -8,11 +10,14 @@ import { Title } from '@angular/platform-browser';
 })
 export class HistoryComponent implements OnInit {
 
-  constructor(private titleService:Title) { 
+  imagenFachadaClub:Observable<any>;
+  constructor(private titleService:Title,
+              private firebaseService:FirebaseService) { 
+
     this.titleService.setTitle("Club Philidor - Historia")
   }
 
   ngOnInit() {
+    this.imagenFachadaClub = this.firebaseService.getMultimedia("historia", "fachada-club.jpg");
   }
-
 }
