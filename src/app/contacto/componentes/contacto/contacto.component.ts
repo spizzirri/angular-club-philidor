9cc023f8aa890f63bfca236a272c7fe7d76ed3cd
Damian <damianspizzirri@gmail.com>
2020-02-23 23:20:41 -0300

    Agrega comentarios en la vista de contacto

--- src/app/contacto/componentes/contacto/contacto.component.ts
+++ src/app/contacto/componentes/contacto/contacto.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { FirebaseService } from 'src/app/core/servicios/firebase.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-contacto',
@@ -8,11 +10,15 @@ import { Title } from '@angular/platform-browser';
 })
 export class ContactoComponent implements OnInit {
 
-  constructor(private titleService:Title) { 
+  private comentarios:Observable<any>
+
+  constructor(private titleService:Title,
+              private firebaseService: FirebaseService) { 
     this.titleService.setTitle("Club Philidor - Contacto");
   }
 
   ngOnInit() {
+    this.comentarios = this.firebaseService.getDocumentos("comentarios", 15)
   }
 
 }
